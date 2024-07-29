d886ca0a61c85edc7ec47407fba83a9119bf07e7
Victor Leonardi <victorleonardireis@poli.ufrj.br>
2024-07-29 01:48:51 -0300

    Add button and logic to work

--- firstStudy/frontend/src/App.vue
+++ firstStudy/frontend/src/App.vue
@@ -1,39 +1,66 @@
 <template>
-  <div class="app">
-    <div class="header">
+  <div class="app bg-gray-200">
+    <div class="header flex">
       <div class="logo-name">
         <img
           class="logo"
           src="https://png.pngtree.com/png-vector/20230531/ourmid/pngtree-train-coloring-page-with-a-locomotive-in-the-background-vector-png-image_6775523.png"
           alt=""
         />
-        <h1 class="brand-name">Game Library</h1>
+        <h1 class="brand-name text-">Game Library</h1>
       </div>
+      <Button class="add-game" @click="handleClick" raised severity="success"
+        >Adicionar Novo Jogo</Button
+      >
     </div>
     <GameTab />
+    <div v-show="showRegisterGame" class="bg-black text-gray-300 text-center py-10 m-10">{{showRegisterGame}}</div>
   </div>
 </template>
 
 <script setup lang="ts">
-import GameTab from './components/GameTab.vue'
+import GameTabPrime from './components/GameTabPrime.vue'
+import Button from 'primevue/button'
+import GameTab from '@/components/GameTab.vue'
+import { ref } from 'vue'
+
+const showRegisterGame = ref(false)
+
+const handleClick = () => {
+  console.log('click')
+  showRegisterGame.value = !showRegisterGame.value
+  console.log(showRegisterGame.value)
+}
 </script>
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Playwrite+BE+VLG:wght@100..400&display=swap');
 
 .app {
-  background-color: antiquewhite;
   height: 100vh;
+}
+
+.header {
+  gap: 40vw;
 }
 
 .logo-name {
   display: flex;
   flex-direction: row;
   align-items: center;
+  width: 50vw;
 }
 
 .logo {
-  width: 15%;
+  width: 35%;
+}
+
+.add-game {
+  width: 300px;
+  height: 4vh;
+  top: 3vh;
+  right: 2%;
+  font-size: x-small;
 }
 
 .brand-name {
