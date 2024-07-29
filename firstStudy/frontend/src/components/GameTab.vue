bc67956d16d4388716f68e573c6a81005b2dc324
Victor Leonardi <victorleonardireis@poli.ufrj.br>
2024-07-29 01:49:14 -0300

    Fix gametab

--- firstStudy/frontend/src/components/GameTab.vue
+++ firstStudy/frontend/src/components/GameTab.vue
@@ -1,49 +1,35 @@
 <template>
-  <table class="game-lib-table">
-    <thead>
-      <tr class="header">
-        <th>Name</th>
-        <th>Genre</th>
-        <th>Played</th>
-        <th>Actions</th>
-      </tr>
-    </thead>
-
-    <tbody>
-      <tr class="row-values" v-for="game in gameLib" :key="game.name">
-        <td>{{ game.name }}</td>
-        <td>{{ game.genre }}</td>
-        <td>{{ game.played }}</td>
-        <td>
-          <button>Update</button>
-          <button @click="removeGameFromLibrary(game.id)">Deletar</button>
-        </td>
-      </tr>
-    </tbody>
-  </table>
-
-  <!-- <div class="container">
-    <div class="header">
-      <h1>Name</h1>
-      <h1>Genre</h1>
-      <h1>Played</h1>
-      <h1>Actions</h1>
-    </div>
-    <div class="row-values" v-for="game in gameLib" :key="game.id">
-      <h1>{{ game.name }}</h1>
-      <h1>{{ game.genre }}</h1>
-      <h1>{{ game.played }}</h1>
-      <div class="action">
-        <button>Update</button>
-        <button @click="removeGameFromLibrary(game.id)">Deletar</button>
-      </div>
-    </div> -->
-  <!-- </div> -->
+  <div class="relative border rounded-lg" id="container">
+    <table class="w-full text-sm text-left text-gray-500" id="table">
+      <thead class="text-s text-white bg-opacity-70 uppercase bg-black">
+        <tr>
+          <th class="px-4 py-3">ID</th>
+          <th class="px-4 py-3">Name</th>
+          <th class="px-4 py-3">Genre</th>
+          <th class="px-4 py-3">Played</th>
+          <th class="px-4 py-3">Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr id="row" v-for="game in gameLib" :key="game.id" class="border-b bg-white">
+          <td class="px-4 py-3 font-bold text-gray-900">{{ game.id }}</td>
+          <td class="px-4 py-3 font-bold text-gray-900">{{ game.name }}</td>
+          <td class="px-4 py-3">{{ game.genre }}</td>
+          <td class="px-4 py-3">{{ game.played }}</td>
+          <td class="px-4 py-3">
+            <Button severity="secondary" text>Update</Button>
+            <Button severity="danger" text @click="removeGameFromLibrary(game.id)">Delete</Button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { get_lib, delete_game } from '@/service/server'
 import { onMounted, ref } from 'vue'
+import Button from 'primevue/button'
 
 const gameLib = ref({})
 
@@ -67,45 +53,18 @@ onMounted(async () => {
 })
 </script>
 
-<style scoped>
-.game-lib-table {
-  width: 90%;
-  padding-left: 10%;
+<style>
+#container {
+  width: 90vw;
+  padding-left: 10vw;
 }
 
-.action {
-  display: flex;
-  justify-content: center;
+#table {
+  border-collapse: collapse;
+  border-radius: 1rem;
 }
 
-.header,
-.row-values {
-  display: flex;
-  justify-content: space-between; /* Distribui espaço entre as colunas */
-  justify-items: start;
-}
-
-.header,
-.row-values,
-.action {
-  width: 100%;
-  flex: 1; /* Cada item ocupará uma parte igual do espaço disponível */
-  text-align: center; /* Alinha o texto ao centro de cada coluna */
-  margin: 0; /* Remove margens adicionais */
-}
-
-.header {
-  font-weight: bold; /* Torna o texto do cabeçalho mais proeminente */
-  border-bottom: solid 3px;
-  font-size: 1.5rem;
-}
-
-.row-values {
-  border-bottom: solid 2px;
-  font-size: 0.6rem;
-}
-
-.row-values:last-child {
+#row:last-child {
   border-bottom: none;
 }
 </style>
